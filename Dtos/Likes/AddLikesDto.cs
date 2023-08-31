50d7338370ca53ee8c1378809512d06c196b1bb3
Fabio <f.massa5@libero.it>
2023-08-31 02:28:31 +0200

    modificato sezione modello like e dto

--- Dtos/Likes/AddLikesDto.cs
+++ Dtos/Likes/AddLikesDto.cs
@@ -5,6 +5,9 @@ namespace betacomio.Dtos.Likes
        
         public string ProductName {get; set;} 
         public string Price {get; set;}
+        public int ProductId { get; set; }
+        public string CategoryName {get; set;}
+
         public int UserId {get; set;}
 
 
--- Models/Like.cs
+++ Models/Like.cs
@@ -8,7 +8,9 @@ namespace betacomio.Models
         [Key]
         public int IdLike {get; set;}
         public string ProductName {get; set;} 
+        public int ProductId {get; set;} 
         public string Price {get; set;}
+        public string CategoryName {get; set;}
 
         public User? User { get; set; }
 
